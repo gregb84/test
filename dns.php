accaa1e4544b316e468992e49822bc47a86c644e
gregb84 <143930347+gregb84@users.noreply.github.com>
2024-02-21 17:15:56 +0000

    Add files via upload

---
+++ dns.php
@@ -0,0 +1,2 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"></head><body>{"status":true,"su":"http://hexatv.watch:8880,http://apnaiptv.asia:80","sc":"","ndd":""}</body></html>
